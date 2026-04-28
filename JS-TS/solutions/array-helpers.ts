47feecf52ee668b4e4e04b18fc238f33077f15a7
Diana <shaleikodiana@gmail.com>
2026-04-28 20:18:01 +0300

    array-helpers.ts

--- JS-TS/solutions/array-helpers.ts
+++ JS-TS/solutions/array-helpers.ts
@@ -1,23 +1,67 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-// Task 02: Mini functional–utility library
-// All helpers are declared but not implemented.
 
 export function mapArray<T, R>(source: readonly T[], mapper: (item: T, index: number) => R): R[] {
-  throw new Error('mapArray: not implemented');
+  if(source==null){
+  throw new TypeError('Source cannot be null or undefined');}
+  const result:R[]=[];
+  for(let i=0;i<source.length;i++){
+const newArr=mapper(source[i],i);
+result.push(newArr);
+  }
+  return result;
 }
 
 export function filterArray<T>(source: readonly T[], predicate: (item: T, index: number) => boolean): T[] {
-  throw new Error('filterArray: not implemented');
+ if(source==null){
+  throw new TypeError('Source cannot be null or undefined');}
+  const result:T[]=[];
+  for(let i=0;i<source.length;i++){
+   const True= predicate(source[i],i);
+    if(True){
+
+    result.push(source[i]);}
+  }
+  return result;
 }
 
 export function reduceArray<T, R>(source: readonly T[], reducer: (acc: R, item: T, index: number) => R, initial: R): R {
-  throw new Error('reduceArray: not implemented');
+  if(source==null){
+  throw new TypeError('Source cannot be null or undefined');}
+  let acc=initial;
+  for(let i=0;i<source.length;i++){
+    acc= reducer(acc,source[i],i);
+    
+  }
+  return acc;
 }
 
 export function partition<T>(source: readonly T[], predicate: (item: T) => boolean): [T[], T[]] {
-  throw new Error('partition: not implemented');
+  if(source==null){
+  throw new TypeError('Source cannot be null or undefined');}
+  const pass: T[] = [];
+const fail: T[] = [];
+for(let i=0;i<source.length;i++){
+    const New=predicate(source[i]);
+    if(New){
+      pass.push(source[i]);
+    }
+    else{
+      fail.push(source[i]);
+    }
+    
+  }
+  return [pass,fail];
 }
 
 export function groupBy<T, K extends PropertyKey>(source: readonly T[], keySelector: (item: T) => K): Record<K, T[]> {
-  throw new Error('groupBy: not implemented');
+   if(source==null){
+  throw new TypeError('Source cannot be null or undefined');}
+  const result = {} as Record<K, T[]>;
+  for(let i=0;i<source.length;i++){
+   const key = keySelector(source[i]);
+   if (!result[key]) {
+    result[key] = [];
+}
+    result[key].push(source[i]);
+  }
+  return result;
 }
