d9b68f002335c834aada6ba45d7d9655b5f78309
Diana <shaleikodiana@gmail.com>
2026-04-28 20:15:44 +0300

    todo-manager.ts

--- JS-TS/solutions/todo-manager.ts
+++ JS-TS/solutions/todo-manager.ts
@@ -1,23 +1,28 @@
-import { TodoService } from './todo-service';
 import { TodoApi } from './todo-api';
-import { Todo } from './types';
+import { TodoService } from './todo-service';
+import { Todo, TodoStatus } from './types';
 
 export class ToDoManager {
-  private service = new TodoService(new TodoApi());
+  private api: TodoApi;
+  private service: TodoService;
 
+  constructor() {
+    this.api = new TodoApi();
+    this.service = new TodoService(this.api);
+  }
   async init(): Promise<void> {
-    throw new Error('init: not implemented');
+    await this.service.create('Learn TypeScript', 'Finish the pre-training course');
+    await this.service.create('Drink coffee', 'Get an energy boost');
   }
-
-  async add(title: string, description = ''): Promise<void> {
-    throw new Error('add: not implemented');
+  async add(title: string, description?: string): Promise<void> {
+    await this.service.create(title, description);
   }
-
   async complete(id: number): Promise<void> {
-    throw new Error('complete: not implemented');
+    await this.api.update(id, { status: TodoStatus.COMPLETED });
   }
+
 
   async list(): Promise<Todo[]> {
-    throw new Error('list: not implemented');
+    return this.api.getAll();
   }
 }
