9a9f109f0c6579ea11894487fbefa595a3e5b23f
Diana <shaleikodiana@gmail.com>
2026-04-28 20:15:59 +0300

    todo-factory.ts

--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -1,7 +1,19 @@
+
 import { Todo, NewTodo, TodoStatus } from './types';
 
 let nextId = 1;
 
+
 export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+
+
+  const newTodo: Todo = {
+    ...input,             
+    id: nextId,        
+    createdAt: new Date(),   
+    status: TodoStatus.PENDING 
+  };
+
+nextId++
+return newTodo;
 }
